1b660bd7783628a24137c0d61d5fcef2ca3c3deb
Hyancinth <ethan.benj.tran@gmail.com>
2026-03-03 17:04:40 -0500

    First implementation of the mpc

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # CA-Deep-Imitation-Learning
 DNN that learns to imitate an MPC for collision avoidance 
 
-## Todo:
+## Todo: 
+Visualize the robot joint motion
+- Understand what's going on 
+Should consider adding more circles along the robot link
+
+## Complete:
 Calculate cartesian velocities for SSM constraint
 - Calculate Jacobian for each joint
 - Consider implementing zeta parameter
+
+
--- simpleMPC.py
+++ simpleMPC.py
@@ -1,6 +1,10 @@
 import numpy as np
 import do_mpc
+from do_mpc.data import save_results, load_results
 import casadi as ca
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 def fk(theta, a):
     """
@@ -20,7 +24,30 @@ def fk(theta, a):
 
     return [x1, y1, x2, y2]
 
-def mpc_controller(a1 = 1.0, a2 = 1.0, Ts=0.05, target=np.array([1.5, 1.5]), obstacle=np.array([1.0, 1.0])):
+def jacobian(theta, a):
+    """
+    Calculate jacobian matrix for each joint of a 2-link RR manipulator
+    """
+    a1 = a[0]
+    a2 = a[1]
+    theta1 = theta[0]
+    theta2 = theta[1]
+
+    j1 = ca.vertcat(
+        ca.horzcat(-a1*ca.sin(theta1), 0),
+        ca.horzcat(a1*ca.cos(theta1), 0)
+    )
+
+    j2 = ca.vertcat(
+        ca.horzcat(-a1*ca.sin(theta1) - a2*ca.sin(theta1 + theta2), -a2*ca.sin(theta1 + theta2)),
+        ca.horzcat(a1*ca.cos(theta1) + a2*ca.cos(theta1 + theta2), a2*ca.cos(theta1 + theta2))
+    )
+
+    return j1, j2
+
+save_pos = []
+
+def mpc_controller(a1 = 1.0, a2 = 1.0, Ts=0.1, target=np.array([1.5, 0.5]), obstacle=np.array([1.0, 0.3])):
     model = do_mpc.model.Model('discrete')
 
     # States (x)
@@ -37,27 +64,28 @@ def mpc_controller(a1 = 1.0, a2 = 1.0, Ts=0.05, target=np.array([1.5, 1.5]), obs
     model.set_rhs('theta1', theta1 + u1*Ts)
     model.set_rhs('theta2', theta2 + u2*Ts)
 
-    # Parameters
-    alpha = 0.89 # SSM 
-    d_ = 0.3 # distance leeway for SSM
-
-    gamma = 500 # repulsion weight in cost function
-    beta = 3 # weight for repulsion function
-
     model.setup() 
 
     # MPC controller
     mpc = do_mpc.controller.MPC(model)
     setup_mpc = {
-        'n_horizon': 10, 
+        'n_horizon': 100, 
         't_step': Ts, 
         'store_full_solution': True,
     }
 
     mpc.set_param(**setup_mpc)
     
+    # Tuning parameters
+    alpha = 0.89 # SSM 
+    d_ = 0.05 # distance leeway for SSM
+
+    gamma = 10 # repulsion weight in cost function
+    beta = 3 # weight for repulsion function
+
+
     # Distances
-    pos = fk([model.aux['theta1'], model.aux['theta2']], [a1, a2]) # joint positions
+    pos = fk([model.x['theta1'], model.x['theta2']], [a1, a2]) # joint positions
     x1 = pos[0]
     y1 = pos[1]
     x2 = pos[2]
@@ -71,14 +99,118 @@ def mpc_controller(a1 = 1.0, a2 = 1.0, Ts=0.05, target=np.array([1.5, 1.5]), obs
     # Cost function 
     epsilon = 1e-6 # prevent division by zero
     mu = ca.exp(-beta*(ee_dist_to_obs**2/(ee_dist_to_target**2 + epsilon)))
-    cost = ee_dist_to_target**2 + (model.aux['u1']**2 + model.aux['u2']**2) + gamma*mu**2
-    mpc.set_objective(cost)
+    
+    cost = ee_dist_to_target**2 + (model.u['u1']**2 + model.u['u2']**2) + gamma*mu**2
+    mterm = ca.DM.zeros(1,1)
+    mpc.set_objective(lterm=cost, mterm=mterm)
+
+    mpc.set_rterm(u1=0.1, u2=0.1)
+
+    # Modeling
+    obs_radius = 0.1
+    joint_radius = 0.2
 
     # Constraints
-    obs_radius = 0.2
-    joint_radius = 0.1
 
-    ssm_1 = alpha**2 *(j1_dist_to_obs**2 - (obs_radius + joint_radius + d_)**2)
-    ssm_2 = alpha**2 *(ee_dist_to_obs**2 - (obs_radius + joint_radius + d_)**2)
+    # SSM constraints
+    j1, j2 = jacobian([model.x['theta1'], model.x['theta2']], [a1, a2])
+    v_j1 = j1 @ ca.vertcat(model.u['u1'], model.u['u2']) # cartesian velocity vector of joint 1
+    v_j2 = j2 @ ca.vertcat(model.u['u1'], model.u['u2']) # cartesian velocity vector of end effector
+
+    zeta_1 = alpha**2 *(j1_dist_to_obs**2 - (obs_radius + joint_radius + d_)**2) # SSM constraint for joint 1
+    zeta_2 = alpha**2 *(ee_dist_to_obs**2 - (obs_radius + joint_radius + d_)**2) # SSM constraint for end effector
+
+    v1 = v_j1[0]**2 + v_j1[1]**2 # velocity magnitude squared of joint 1
+    v_ee = v_j2[0]**2 + v_j2[1]**2 # velocity magnitude squared of end effector
+
+    mpc.set_nl_cons('ssm1', v1 - zeta_1, ub = 0)
+    mpc.set_nl_cons('ssm2', v_ee - zeta_2, ub = 0)
+
+    # joint angle constraints
+    mpc.bounds['lower','_x','theta1'] = -ca.pi
+    mpc.bounds['upper','_x','theta1'] = ca.pi
+
+    mpc.bounds['lower','_x','theta2'] = -ca.pi
+    mpc.bounds['upper','_x','theta2'] = ca.pi
+
+    # joint velocity constraints
+    mpc.bounds['lower','_u','u1'] = -3.0 # rad/s
+    mpc.bounds['upper','_u','u1'] = 3.0 
     
-mpc_controller()
+    mpc.bounds['lower','_u','u2'] = -3.0 # rad/s
+    mpc.bounds['upper','_u','u2'] = 3.0 
+
+    mpc.setup()
+
+    # Simulator
+    simulator = do_mpc.simulator.Simulator(model)
+    simulator.settings.t_step = Ts
+    simulator.setup()
+
+    print("MPC and Simulator setup complete.")
+    return mpc, simulator   
+
+# def robot_link_lines(x):
+#     x = x.flatten()
+#     a = [1.0, 1.0]
+
+
+def simulate_and_visualize():
+    mpc, simulator = mpc_controller()
+
+    simulator.reset_history()
+    mpc.reset_history()
+    
+    x0 = np.array([ca.pi/6, ca.pi/6]) # initial joint angles
+
+    mpc.x0 = x0
+    simulator.x0 = x0
+
+    mpc.set_initial_guess()
+
+    # open loop simulation
+    # u0 = mpc.make_step(x0)
+
+    # Simulate
+    for i in range(100):
+        u0 = mpc.make_step(x0)
+        x0 = simulator.make_step(u0)
+    
+    # save_results([mpc, simulator])
+    
+    mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
+    sim_graphics = do_mpc.graphics.Graphics(simulator.data)
+
+    fig, ax = plt.subplots(2, sharex = True, figsize=(16,9))
+    fig.align_ylabels()
+
+    for g in [sim_graphics, mpc_graphics]:
+        g.add_line(var_type='_x', var_name='theta1', axis=ax[0], color = '#1f77b4')
+        g.add_line(var_type='_x', var_name='theta2', axis=ax[0], color = '#ff7f0e')
+        g.add_line(var_type='_u', var_name='u1', axis=ax[1], color = '#1f77b4')
+        g.add_line(var_type='_u', var_name='u2', axis=ax[1], color = '#ff7f0e')
+    
+    ax[0].set_ylabel('Joint Angles (rad)')
+    ax[1].set_ylabel('Joint Velocities (rad/s)')
+    ax[1].set_xlabel('Time (s)')
+
+    for line_i in mpc_graphics.pred_lines['_x', 'theta1']: line_i.set_color('#1f77b4')
+    for line_i in mpc_graphics.pred_lines['_x', 'theta2']: line_i.set_color('#ff7f0e')
+    for line_i in mpc_graphics.pred_lines['_u', 'u1']: line_i.set_color('#1f77b4')
+    for line_i in mpc_graphics.pred_lines['_u', 'u2']: line_i.set_color('#ff7f0e')
+
+    for line_i in mpc_graphics.pred_lines.full: line_i.set_alpha(0.2)
+
+    lines = sim_graphics.result_lines['_x', 'theta1'] + sim_graphics.result_lines['_x', 'theta2']
+    ax[0].legend(lines, ['theta1', 'theta2'], title = 'Joint Angles (rad)')
+
+    lines = sim_graphics.result_lines['_u', 'u1'] + sim_graphics.result_lines['_u', 'u2']
+    ax[1].legend(lines, ['u1', 'u2'], title = 'Joint Velocities (rad/s)')
+
+    mpc_graphics.plot_predictions(t_ind=0)
+    sim_graphics.plot_results()
+    sim_graphics.reset_axes()
+    plt.show()
+
+if __name__ == "__main__":
+    simulate_and_visualize()
