d7eb05fb0c49ca5fce05667bcdc0f2949ed8ed21
Hyancinth <ethan.benj.tran@gmail.com>
2026-03-11 23:50:34 -0400

    Process and split data into test and train

---
+++ data/model_data.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+X_COLUMNS = [
+    'theta1',
+    'theta2',
+    'target_x',
+    'target_y',
+    'obstacle_x',
+    'obstacle_y',
+    'ee_dist_to_target',
+    'ee_dist_to_obstacle',
+    'min_dist_obstacle_link_1',
+    'min_dist_obstacle_link_2',
+    'u1_prev',
+    'u2_prev'
+]
+
+Y_COLUMNS = [
+    'u1', 
+    'u2'
+]
+
+def split_test_train(data, test_size=0.2):
+    """
+    Split data dict into training and testing sets
+
+    load_data_from_file should be called before this to load the data from the h5 file into a dict
+    This should be called before generating input-output pairs to prevent data leakage between train and test sets
+    """
+    keys = list(data.keys()) # list of all runs
+    train_keys, test_keys = train_test_split(keys, test_size=test_size, random_state=42) # split runs into train and test sets
+
+    train_data = {k: data[k] for k in train_keys}
+    test_data = {k: data[k] for k in test_keys}
+
+    return train_data, test_data
+
+def generate_input_output_data(data):
+    """
+    Convert data dict to array of input features (X) and outputs (Y)
+
+    Returns:
+    - x (numpy array): array of input features for all runs, shape (num_samples, num_features)
+    - y (numpy array): array of outputs for all runs, shape (num_samples, num_outputs)
+    """
+    x = []
+    y = []
+    for run in data.keys():
+        run_data = data[run]
+        x_run = np.column_stack([run_data[col] for col in X_COLUMNS])
+        y_run = np.column_stack([run_data[col] for col in Y_COLUMNS])
+
+        x.append(x_run)
+        y.append(y_run)
+    
+    return np.vstack(x), np.vstack(y)
+
+    
+if __name__ == "__main__":
+    from data.load_data import load_data_from_file
+    file_path = "model/data/test_data_1.h5"
+    data = load_data_from_file(file_path) # only two runs 
+    x, y = generate_input_output_data(data)
+    print("Input features (X) shape: ", x.shape)
+    print("Output labels (Y) shape: ", y.shape)
+    print("First 5 rows of X: \n", x[:5])
+    
+
--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -65,7 +65,7 @@ def run_mpc_controller(mpc, simulator, p_template, p_template_sim, target, obsta
 if __name__ == "__main__":
     mpc, simulator, p_template, p_template_sim = mpc_controller()
 
-    num_runs = 30
+    num_runs = 2
     run = 0
 
     while run < num_runs:
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         if success:
             print(f"Run {run+1}/{num_runs} successful. Saving data...")
             data['run_number'] = run
-            write_data_to_file(data, 'data_311_01_30.h5')
+            write_data_to_file(data, 'test_data_1.h5')
             run += 1
         else:
             print(f"Run {run+1} failed. Retrying with a new target and obstacle...")
